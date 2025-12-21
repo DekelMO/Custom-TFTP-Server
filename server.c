40abb1d446a030b9ca8963a8f79fb3347c083b4a
DekelMO <Dekelm94@gmail.com>
2025-12-21 06:21:26 +0200

    complet RRQ server handling

--- server.c
+++ server.c
@@ -1,6 +1,11 @@
-#include "udp_file_transfer.h"
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include "udp_file_transfer.h"
+
+
 
 int main()
 {
@@ -12,7 +17,7 @@ int main()
     struct sockaddr_in servaddr;
     memset(&servaddr, 0, sizeof(servaddr));//rest garb values
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(6969);
+    servaddr.sin_port = htons(SERVER_PORT);
     servaddr.sin_addr.s_addr = INADDR_ANY;
 
     if(bind(socketfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
@@ -23,16 +28,117 @@ int main()
 
     Packet_t msg_in;
     struct sockaddr_in client_addr;
-    socklen_t client_len = sizeof(client_addr);
+    socklen_t client_len;
 
     while(1)
     {
         ssize_t bytes_received;
+        client_len = sizeof(client_addr);
         bytes_received = recvfrom(socketfd, &msg_in, sizeof(msg_in), 0, (struct sockaddr *)&client_addr, &client_len);
-        if (bytes_received > 0)
+        if (bytes_received > HEADER_SIZE)// received msg and its not empty
         {
-            uint16_t opcode = msg_in.opcode;
-            uint16_t id = msg_in.id;
+            uint16_t opcode = ntohs(msg_in.opcode);
+            uint16_t id = ntohs(msg_in.id);
+
+            switch (opcode)
+            {
+            case 1: // RRQ
+                printf("RRQ received for file %s\n", msg_in.payload.filename);
+                FILE* file = fopen(msg_in.payload.filename, "rb");
+                if(!file)
+                {
+                    Packet_t err_packet;
+                    memset(&err_packet, 0, sizeof(err_packet));
+                    err_packet.opcode = htons(OP_ERROR);
+                    err_packet.id = htons(ERR_NOT_FOUND); 
+                    strncpy(err_packet.payload.error_msg, "File not Found", MAX_ERROR_MSG - 1);
+                    err_packet.payload.error_msg[MAX_ERROR_MSG - 1] = '\0';
+                    
+                    int err_len = HEADER_SIZE + strlen(err_packet.payload.error_msg) + 1;
+                    sendto(socketfd, &err_packet, err_len, 0, (struct sockaddr *)&client_addr, client_len);
+                    perror("File not found");
+                }
+                else
+                {
+                    uint16_t block_counter = 1;
+                    Packet_t data_packet;
+                    data_packet.opcode = htons(OP_DATA);
+                    size_t bytes_read = 0;
+                    bool read_next = true;
+                    bool finish = false;
+
+                    while(1)
+                    {
+                        if(read_next)
+                        {
+                            bytes_read = fread(data_packet.payload.data, 1, MAX_PAYLOAD_SIZE, file);
+                            
+                            if (ferror(file))
+                            {
+                                Packet_t err_packet;
+                                err_packet.opcode = htons(OP_ERROR);
+                                err_packet.id = htons(READDING_ERROR); 
+                                strncpy(err_packet.payload.error_msg, "Reading file error", MAX_ERROR_MSG - 1);
+                                err_packet.payload.error_msg[MAX_ERROR_MSG - 1] = '\0';
+                                
+                                int err_len = HEADER_SIZE + strlen(err_packet.payload.error_msg) + 1;
+                                sendto(socketfd, &err_packet, err_len, 0, (struct sockaddr *)&client_addr, client_len);
+                                break; 
+                            }
+
+                            if (bytes_read < MAX_PAYLOAD_SIZE)
+                            {
+                                finish = true;
+                            }
+
+                            data_packet.id = htons(block_counter);
+                            read_next = false; 
+                        }
+
+                        sendto(socketfd, &data_packet, bytes_read + HEADER_SIZE, 0, (struct sockaddr *)&client_addr, client_len);
+                        
+                        bytes_received = recvfrom(socketfd, &msg_in, sizeof(msg_in), 0, (struct sockaddr *)&client_addr, &client_len);
+                        
+                        if (bytes_received >= HEADER_SIZE)
+                        {
+                            if (ntohs(msg_in.opcode) == OP_ACK)
+                            {
+                                if (ntohs(msg_in.id) == block_counter)
+                                {
+                                    if (finish)
+                                    {
+                                        break; 
+                                    }
+                                    block_counter++;
+                                    read_next = true;
+                                }
+                            }
+                        }
+                    }
+                    fclose(file);
+                    printf("Transfer finished.\n");
+                }
+                break;
+            case 2://WRQ
+                printf("WRQ received for file %s\n", msg_in.payload.filename);
+                handle_wrq(socketfd, &client_addr, client_len, &msg_in);
+                break;
+            case 3://DATA
+                /* code */
+                break;
+            case 4://ACK
+                /* code */
+                break;
+            case 5://ERROR
+                /* code */
+                break;
+            case 6://DELETE
+                /* code */
+                break;
+            
+            default:
+                break;
+            }
 
         }
     }
--- udp_file_transfer.h
+++ udp_file_transfer.h
@@ -1,13 +1,33 @@
 #include <stdint.h>
 
+#define SERVER_PORT     6969    
+#define MAX_PAYLOAD_SIZE 512    
+#define MAX_FILENAME    256     
+#define MAX_ERROR_MSG   128  
+#define HEADER_SIZE 4
+
+#define OP_RRQ    1  // Read Request
+#define OP_WRQ    2  // Write Request
+#define OP_DATA   3  // Data 
+#define OP_ACK    4  // Acknowledgment
+#define OP_ERROR  5  // Error
+#define OP_DELETE 6  // Delete
+
+#define ERR_NOT_FOUND     1  
+#define ERR_ACCESS_DENIED 2 
+#define ERR_DISK_FULL     3  
+#define ERR_ILLEGAL_OP    4  
+#define ERR_FILE_EXISTS   5
+#define READDING_ERROR    6
+
 typedef struct
 {
    uint16_t opcode;
    uint16_t id;
    union 
    {
-    char data[2048];
-    char filename[2048];
-    char error_msg[2048];
+    char data[MAX_PAYLOAD_SIZE];
+    char filename[MAX_FILENAME];
+    char error_msg[MAX_ERROR_MSG];
    } payload;
 } __attribute__((packed)) Packet_t;
